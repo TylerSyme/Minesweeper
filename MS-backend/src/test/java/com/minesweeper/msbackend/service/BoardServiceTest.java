a6062a8594bab627ba2ff696e852a82e821e4db6
TylerSyme <tylersyme12@gmail.com>
2023-05-19 21:30:54 -0400

    Add toggle flag tests

--- MS-backend/src/test/java/com/minesweeper/msbackend/service/BoardServiceTest.java
+++ MS-backend/src/test/java/com/minesweeper/msbackend/service/BoardServiceTest.java
@@ -2,9 +2,10 @@ package com.minesweeper.msbackend.service;
 
 import com.minesweeper.msbackend.model.Board;
 import com.minesweeper.msbackend.model.Cell;
+import com.minesweeper.msbackend.model.Coordinate;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BoardServiceTest {
 
@@ -48,5 +49,29 @@ class BoardServiceTest {
 
         assertEquals(width, board.getCells().length);
         assertEquals(numMines, mineCount);
+    }
+
+    @Test
+    void toggleFlag() {
+
+        final int width = 1;
+        final int numMines = 0;
+        Board board = boardService.createBoard(width, numMines);
+
+        assertFalse(board.getCells()[0][0].isFlagged());
+        boardService.toggleFlag(new Coordinate(0, 0));
+        assertTrue(board.getCells()[0][0].isFlagged());
+    }
+
+    @Test
+    void toggleFlagMine() {
+
+        final int width = 1;
+        final int numMines = 1;
+        Board board = boardService.createBoard(width, numMines);
+
+        assertFalse(board.getCells()[0][0].isFlagged());
+        boardService.toggleFlag(new Coordinate(0, 0));
+        assertFalse(board.getCells()[0][0].isFlagged());
     }
 }
