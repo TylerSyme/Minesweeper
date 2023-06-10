c883ddac75391d0b79c20e18e8bb97c41ae40cf6
TylerSyme <tylersyme12@gmail.com>
2023-06-10 15:35:59 -0400

    Refactor start screen animations

--- MS-frontend/src/components/StartScreen.vue
+++ MS-frontend/src/components/StartScreen.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import {backendBaseUrl} from "../main.ts";
+import ExpandTransition from "./Transitions/ExpandTransition.vue";
 
 const emit = defineEmits(["createGame"]);
 
@@ -11,87 +12,68 @@ enum Difficulty {
   custom = "custom",
 }
 
-const selectedDifficulty = ref<Difficulty | undefined>(undefined);
-const boardWidth = ref<number>(1);
-const numMines = ref<number>(1);
+const boardWidth = ref<number>(10);
+const numMines = ref<number>(10);
 const showDifficulties = ref<boolean>(true);
 
-function startGame() {
-  let requestUrl = backendBaseUrl + "/api/difficulty/" + selectedDifficulty.value;
-  if (selectedDifficulty.value as string === "custom") {
+function startGame(mode: Difficulty) {
+  let requestUrl = backendBaseUrl + "/api/difficulty/" + mode as string;
+  if (mode as string === "custom") {
     requestUrl = backendBaseUrl + "/api/custom?size=" + boardWidth.value + "&numMines=" + numMines.value;
   }
   emit("createGame", requestUrl);
 }
-
-function resetElementHeight(el) {
-  el.style.height = el.scrollHeight + "px";
-}
-
-function clearElementHeight(el) {
-  el.style.height = "";
-}
 </script>
 
 <template>
-  <div class="bg-darkgray-900 rounded max-w-lg w-full p-24 bg-opacity-30 shadow-lg backdrop-blur border-darkgray-900">
+  <div class="bg-darkgray-900 rounded-lg max-w-lg w-full p-24 bg-opacity-30 shadow-lg backdrop-blur border-darkgray-800 border">
     <h1 class="font-display text-5xl text-gray-200 text-center pb-12">Minesweeper</h1>
-    <div>
-      <Transition name="expand" @enter="resetElementHeight" @after-enter="clearElementHeight" @before-leave="resetElementHeight" @after-leave="clearElementHeight">
-        <div v-show="showDifficulties">
-          <div v-for="difficulty in [Difficulty.easy, Difficulty.medium, Difficulty.hard]">
-            <input type="radio" :id="difficulty" :value="difficulty" v-model="selectedDifficulty" class="hidden"/>
-            <label :for="difficulty" class="block rounded w-full p-4 my-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur"
-                   :class="difficulty === selectedDifficulty ? 'bg-red-600' : 'bg-darkgray-800'">{{
-                difficulty.toUpperCase()
-              }}</label>
-          </div>
-        </div>
-      </Transition>
-    </div>
-
-    <div>
-      <input type="radio" :id="Difficulty.custom" :value="Difficulty.custom" v-model="selectedDifficulty" class="hidden" @click="showDifficulties = !showDifficulties"/>
-      <label :for="Difficulty.custom" class="block rounded w-full p-4 my-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur"
-             :class="Difficulty.custom === selectedDifficulty ? 'bg-red-600 border-red-600' : 'bg-darkgray-800 border-darkgray-800'">{{
-          Difficulty.custom.toUpperCase()
-        }}</label>
-      <div v-if="selectedDifficulty === Difficulty.custom"
-           class="flex flex-wrap pb-4">
-        <div class="w-1/2 px-2 box-border">
-          <label for="boardWidth" class="text-gray-200 pb-2 block text-center">Board width</label>
-          <input type="number" id="boardWidth" v-model="boardWidth"
-                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
-        </div>
-        <div class="w-1/2 px-2 box-border">
-          <label for="numMines" class="text-gray-200 pb-2 block text-center">Number of mines</label>
-          <input type="number" id="numMines" v-model="numMines"
-                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
-        </div>
-        <div v-if="boardWidth * boardWidth < numMines" class="w-full pt-2 text-center text-red-500">
-          The board must be large enough to fit all mines
+    <ExpandTransition>
+      <div v-show="showDifficulties">
+        <div v-for="difficulty in [Difficulty.easy, Difficulty.medium, Difficulty.hard]" class="py-2">
+          <button
+              class="block rounded w-full p-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur bg-darkgray-800 border-darkgray-800 border"
+              @click="startGame(difficulty)">{{
+              difficulty.toUpperCase()
+            }}
+          </button>
         </div>
       </div>
+    </ExpandTransition>
+
+    <div>
+      <div class="py-2">
+        <button
+            class="block rounded w-full p-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur bg-darkgray-800 border-darkgray-800 border"
+            @click="showDifficulties = !showDifficulties">{{
+            Difficulty.custom.toUpperCase()
+          }}
+        </button>
+      </div>
+      <ExpandTransition>
+        <div v-show="!showDifficulties" class="flex flex-wrap pb-4">
+          <div class="w-1/2 px-2 box-border">
+            <label for="boardWidth" class="text-gray-200 pb-2 block text-center">Board width</label>
+            <input type="text" id="boardWidth" v-model="boardWidth"
+                   class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
+          </div>
+          <div class="w-1/2 px-2 box-border">
+            <label for="numMines" class="text-gray-200 pb-2 block text-center">Number of mines</label>
+            <input type="text" id="numMines" v-model="numMines"
+                   class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
+          </div>
+          <div v-if="boardWidth * boardWidth < numMines" class="w-full pt-2 text-center text-red-500">
+            The board must be large enough to fit all mines
+          </div>
+          <div class="text-center pt-4">
+            <button type="button" class="bg-red-600 hover:bg-red-700 text-gray-200 py-4 px-8 rounded"
+                    @click="startGame(Difficulty.custom)">
+              Start
+            </button>
+          </div>
+        </div>
+      </ExpandTransition>
     </div>
-    <div class="text-center">
-      <button type="button" class="bg-red-600 hover:bg-red-700 text-gray-200 py-4 px-8 mt-4 rounded" @click="startGame">
-        Start
-      </button>
-    </div>
+
   </div>
 </template>
-
-<style scoped>
-.expand-enter-active,
-.expand-leave-active {
-  will-change: height, opacity;
-  transition: height 1s ease, opacity 0.3s ease;
-  overflow: hidden;
-}
-
-.expand-enter-from,
-.expand-leave-to {
-  height: 0 !important;
-  opacity: 0;
-}
-</style>
---
+++ MS-frontend/src/components/Transitions/ExpandTransition.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+function resetElementHeight(element) {
+  element.style.height = element.scrollHeight + "px";
+}
+
+function clearElementHeight(element) {
+  element.style.height = "";
+}
+</script>
+
+<template>
+  <Transition name="expand" @enter="resetElementHeight" @after-enter="clearElementHeight"
+              @before-leave="resetElementHeight" @after-leave="clearElementHeight">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style>
+.expand-enter-active,
+.expand-leave-active {
+  will-change: height, opacity;
+  overflow: hidden;
+}
+
+.expand-enter-active {
+  transition: height 0.3s ease, opacity 0.4s ease;
+}
+
+.expand-leave-active {
+  transition: height 0.4s ease, opacity 0.3s ease;
+}
+
+.expand-enter-from,
+.expand-leave-to {
+  height: 0 !important;
+  opacity: 0;
+}
+</style>
