81a562db2e31ec64a8fd0c7e3c4fa15abb18fd10
TylerSyme <tylersyme12@gmail.com>
2023-06-10 12:29:14 -0400

    Update start screen with animation

--- MS-frontend/src/App.vue
+++ MS-frontend/src/App.vue
@@ -3,7 +3,9 @@ import Game from "./components/Game.vue";
 </script>
 
 <template>
-  <div class="min-w-screen min-h-screen bg-darkgray-800 flex justify-center items-center overflow-auto">
-    <Game />
+  <div
+      class="min-w-screen min-h-screen bg-darkgray-800 flex justify-center items-center overflow-auto bg-fixed bg-center bg-no-repeat bg-cover"
+      :style="{backgroundImage: `url('/background.png')`}">
+    <Game/>
   </div>
 </template>
--- MS-frontend/src/components/StartScreen.vue
+++ MS-frontend/src/components/StartScreen.vue
@@ -14,6 +14,7 @@ enum Difficulty {
 const selectedDifficulty = ref<Difficulty | undefined>(undefined);
 const boardWidth = ref<number>(1);
 const numMines = ref<number>(1);
+const showDifficulties = ref<boolean>(true);
 
 function startGame() {
   let requestUrl = backendBaseUrl + "/api/difficulty/" + selectedDifficulty.value;
@@ -22,28 +23,50 @@ function startGame() {
   }
   emit("createGame", requestUrl);
 }
+
+function resetElementHeight(el) {
+  el.style.height = el.scrollHeight + "px";
+}
+
+function clearElementHeight(el) {
+  el.style.height = "";
+}
 </script>
 
 <template>
-  <div class="bg-darkgray-900 rounded max-w-lg w-full p-12">
-    <h1 class="font-display text-4xl text-gray-200 text-center pb-4">Minesweeper</h1>
-    <p class="text-gray-300 py-2">Select a difficulty</p>
-    <div v-for="difficulty in Difficulty">
-      <input type="radio" :id="difficulty" :value="difficulty" v-model="selectedDifficulty" class="hidden"/>
-      <label :for="difficulty" class="block rounded w-full p-4 my-4 text-gray-200 cursor-pointer"
-             :class="difficulty === selectedDifficulty ? 'bg-red-600' : 'bg-darkgray-800'">{{
-          difficulty.charAt(0).toUpperCase() + difficulty.slice(1)
+  <div class="bg-darkgray-900 rounded max-w-lg w-full p-24 bg-opacity-30 shadow-lg backdrop-blur border-darkgray-900">
+    <h1 class="font-display text-5xl text-gray-200 text-center pb-12">Minesweeper</h1>
+    <div>
+      <Transition name="expand" @enter="resetElementHeight" @after-enter="clearElementHeight" @before-leave="resetElementHeight" @after-leave="clearElementHeight">
+        <div v-show="showDifficulties">
+          <div v-for="difficulty in [Difficulty.easy, Difficulty.medium, Difficulty.hard]">
+            <input type="radio" :id="difficulty" :value="difficulty" v-model="selectedDifficulty" class="hidden"/>
+            <label :for="difficulty" class="block rounded w-full p-4 my-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur"
+                   :class="difficulty === selectedDifficulty ? 'bg-red-600' : 'bg-darkgray-800'">{{
+                difficulty.toUpperCase()
+              }}</label>
+          </div>
+        </div>
+      </Transition>
+    </div>
+
+    <div>
+      <input type="radio" :id="Difficulty.custom" :value="Difficulty.custom" v-model="selectedDifficulty" class="hidden" @click="showDifficulties = !showDifficulties"/>
+      <label :for="Difficulty.custom" class="block rounded w-full p-4 my-4 text-gray-200 cursor-pointer italic text-center bg-opacity-70 shadow-lg backdrop-blur"
+             :class="Difficulty.custom === selectedDifficulty ? 'bg-red-600 border-red-600' : 'bg-darkgray-800 border-darkgray-800'">{{
+          Difficulty.custom.toUpperCase()
         }}</label>
-      <div v-if="difficulty === Difficulty.custom && selectedDifficulty === Difficulty.custom" class="flex flex-wrap pb-4">
-        <div class="w-1/2 px-4 box-border">
+      <div v-if="selectedDifficulty === Difficulty.custom"
+           class="flex flex-wrap pb-4">
+        <div class="w-1/2 px-2 box-border">
           <label for="boardWidth" class="text-gray-200 pb-2 block text-center">Board width</label>
           <input type="number" id="boardWidth" v-model="boardWidth"
-                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 outline-0 rounded"/>
+                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
         </div>
-        <div class="w-1/2 px-4 box-border">
+        <div class="w-1/2 px-2 box-border">
           <label for="numMines" class="text-gray-200 pb-2 block text-center">Number of mines</label>
           <input type="number" id="numMines" v-model="numMines"
-                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 outline-0 rounded"/>
+                 class="max-w-full w-full bg-darkgray-800 text-gray-200 py-2 px-4 border-0 focus:outline-none rounded"/>
         </div>
         <div v-if="boardWidth * boardWidth < numMines" class="w-full pt-2 text-center text-red-500">
           The board must be large enough to fit all mines
@@ -57,3 +80,18 @@ function startGame() {
     </div>
   </div>
 </template>
+
+<style scoped>
+.expand-enter-active,
+.expand-leave-active {
+  will-change: height, opacity;
+  transition: height 1s ease, opacity 0.3s ease;
+  overflow: hidden;
+}
+
+.expand-enter-from,
+.expand-leave-to {
+  height: 0 !important;
+  opacity: 0;
+}
+</style>
