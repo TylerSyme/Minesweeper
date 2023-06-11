5e7b2a88013ee18d5ac0fc793881baee680bd937
TylerSyme <tylersyme12@gmail.com>
2023-06-10 21:47:11 -0400

    Update board cells and flag count

--- MS-frontend/src/components/GameBoard.vue
+++ MS-frontend/src/components/GameBoard.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
   initialBoard: Board
 }>();
 const board = ref<Board>(props.initialBoard);
-const flagsLeft = ref<number>(props.initialBoard.numMines);
+const flagCount = ref<number>(0);
 const emit = defineEmits(["mineExploded", "gameWon"]);
 
 function toggleFlag(coordinate: Coordinate) {
@@ -24,6 +24,14 @@ function toggleFlag(coordinate: Coordinate) {
     throw new Error(response.statusText);
   }).then(data => {
     (board.value as Board).cells[coordinate.x][coordinate.y].flagged = data;
+    if ((board.value as Board).cells[coordinate.x][coordinate.y].revealed) {
+      return;
+    }
+    if (data) {
+      flagCount.value++;
+    } else {
+      flagCount.value--;
+    }
   }).catch(error => console.log(error));
 }
 
@@ -47,15 +55,25 @@ function revealCell(coordinate: Coordinate) {
     } else if (data.gameWon) {
       emit("gameWon");
     }
+    // Update flag count
+    let newFlagCount: number = 0;
+    board.value.cells.forEach(row => {
+      row.forEach(cell => {
+        if (cell.flagged && !cell.revealed) {
+          newFlagCount++;
+        }
+      });
+    });
+    flagCount.value = newFlagCount;
   }).catch(error => console.log(error));
 }
 </script>
 
 <template>
-  <div class="bg-darkgray-900 rounded p-8 m-16" :style="{width: `${board.width * 48}px`}">
-    <div class="flex justify-center items-center pb-4 text-gray-200">
+  <div class="bg-darkgray-900 rounded-lg p-16 pt-8 bg-opacity-30 shadow-lg backdrop-blur border-darkgray-800 border scroll-p-16" :style="{width: `${board.width * 48}px`}">
+    <div class="flex justify-center items-center pb-8 text-gray-200">
       <span>Total mines: {{ board.numMines }}</span>
-      <span class="pl-8">Flags left: {{ flagsLeft }}</span>
+      <span class="pl-8">Flags left: {{ flagCount }}</span>
     </div>
     <div class="flex flex-wrap">
       <template v-for="(row, rowIndex) in board.cells" :key="rowIndex">
--- MS-frontend/src/components/GameBoardCell.vue
+++ MS-frontend/src/components/GameBoardCell.vue
@@ -6,19 +6,20 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="aspect-square p-1" :style="{width: props.displayWidth + '%'}">
-    <!--    <div v-if="boardCell.flagged" class="bg-red-600 w-full h-full rounded"></div>-->
-    <!--    <div v-else-if="boardCell.revealed" class="bg-darkgray-800 w-full h-full rounded flex justify-center items-center">-->
-    <!--      <div v-if="boardCell.adjacentMines > 0" class="text-gray-200">{{boardCell.adjacentMines}}</div>-->
-    <!--    </div>-->
-    <!--    <div v-else class="bg-darkgray-800 w-full h-full rounded"></div>-->
-
-    <!--    TESTING  -->
-    <div class="w-full h-full flex justify-center items-center"
-         :class="[boardCell.mine ? 'bg-red-600' : 'bg-darkgray-800', boardCell.flagged ? 'rounded-full' : 'rounded', boardCell.revealed ? 'border-2 border-blue-500' : '']"
+  <div class="aspect-square bg-opacity-70 backdrop-blur bg-darkgray-800 select-none"
+       :style="{width: props.displayWidth + '%'}">
+    <div v-if="boardCell.flagged" class="bg-red-600 w-full h-full"
          @click="$emit('revealCell', boardCell.coordinate)"
-         @contextmenu.prevent="$emit('toggleFlag', boardCell.coordinate)">
+         @contextmenu.prevent="$emit('toggleFlag', boardCell.coordinate)"></div>
+    <div v-else-if="boardCell.revealed && boardCell.mine" @contextmenu.prevent
+         class="w-full h-full bg-cyan-500 rounded-full">
+    </div>
+    <div v-else-if="boardCell.revealed" @contextmenu.prevent
+         class="w-full h-full flex justify-center items-center">
       <div v-if="boardCell.adjacentMines > 0" class="text-gray-200">{{ boardCell.adjacentMines }}</div>
     </div>
+    <div v-else class="bg-darkgray-900 w-full h-full"
+         @click="$emit('revealCell', boardCell.coordinate)"
+         @contextmenu.prevent="$emit('toggleFlag', boardCell.coordinate)"></div>
   </div>
 </template>
